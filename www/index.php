9533ec440d21c74a3f00a4222d51a14934273f82
hashstudio <hashstudio@mail.ru>
2017-08-10 11:16:54 +0300

    Debug

--- www/index.php
+++ www/index.php
@@ -5,9 +5,13 @@ include('../app/vendor/autoload.php');
 $configPath = '../app/config/settings.php';
 $localConfigPath = '../app/config/local_settings.php';
 
+$debug = false;
 if (is_file($localConfigPath)) {
     $configPath = $localConfigPath;
+    $debug = true;
 }
+
+define("DEBUG", $debug);
 
 $config = include $configPath;
 \Phact\Main\Phact::init($config);
