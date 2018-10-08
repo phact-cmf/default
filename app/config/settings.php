77a80eef29f85b00383180d838096df82445d36e
Anton Okulov <agency@ordertarget.ru>
2018-10-08 17:16:00 +0300

    Settings up

--- app/config/settings.php
+++ app/config/settings.php
@@ -2,12 +2,6 @@
 
 return [
     'name' => 'First phact application',
-    'paths' => [
-        'base' => realpath(__DIR__ . '/../'),
-        'www' => realpath(__DIR__ . '/../../www'),
-        'static' => realpath(__DIR__ . '/../../www/static'),
-        'static_modules' => realpath(__DIR__ . '/../../www/static_modules'),
-    ],
     'modules' => [
         'Assets',
         'Editor',
@@ -21,86 +15,105 @@ return [
         'Sitemap'
     ],
     'components' => [
-        'db' => [
-            'class' => \Phact\Orm\ConnectionManager::class,
-            'settings' => [
-                'cacheFieldsTimeout' => PHACT_DEBUG ? null : 86400
-            ],
-            'connections' => [
-                'default' => [
-                    'driver' => 'mysql',
-                    'config' => [
-                        'host' => '127.0.0.1',
-                        'database' => 'default',
-                        'username' => 'user',
-                        'password' => 'password',
-                        'charset' => 'utf8', // Optional
-                        'collation' => 'utf8_unicode_ci', // Optional
+        'paths' => [
+            'class' => \Phact\Components\Path::class,
+            'calls' => [
+                'setPaths' => [
+                    [
+                        'base' => realpath(__DIR__ . '/../'),
+                        'www' => realpath(__DIR__ . '/../../www'),
+                        'static' => realpath(__DIR__ . '/../../www/static'),
+                        'static_modules' => realpath(__DIR__ . '/../../www/static_modules'),
+                        'root' => realpath(__DIR__ . '/../../'),
                     ]
                 ]
             ]
         ],
-        'errorHandler' => [
-            'class' => \Phact\Main\ErrorHandler::class,
-            'debug' => PHACT_DEBUG
+        'translate' => [
+            'class' => \Phact\Translate\Translate::class,
+            'calls' => [
+                'setLocale' => ['ru']
+            ]
         ],
-        'log' => [
-            'class' => \Phact\Log\LogManager::class,
-            'handlers' => [
-                'default' => [
-                    'class' => \Monolog\Handler\RotatingFileHandler::class,
-                    '__construct' => [
-                        realpath(__DIR__ . '/../runtime') . '/default.log',
-                        7,
-                        PHACT_DEBUG ? \Monolog\Logger::DEBUG : \Monolog\Logger::WARNING
+        'db' => [
+            'class' => \Phact\Orm\ConnectionManager::class,
+            'properties' => [
+                'settings' => [
+                    'cacheFieldsTimeout' => PHACT_DEBUG ? null : 86400
+                ],
+                'connections' => [
+                    'default' => [
+                        'host' => '127.0.0.1',
+                        'dbname' => 'tpl',
+                        'user' => 'root',
+                        'password' => '',
+                        'charset' => 'utf8',
+                        'driver' => 'pdo_mysql',
                     ]
                 ]
             ],
-            'loggers' => [
-                'default' => [
-                    'class' => \Monolog\Logger::class,
-                    'handlers' => [
-                        'default'
-                    ]
-                ]
+        ],
+        'errorHandler' => [
+            'class' => \Phact\Main\ErrorHandler::class,
+            'properties' => [
+                'debug' => PHACT_DEBUG
+            ]
+        ],
+        'logger.default' => [
+            'class' => \Monolog\Logger::class,
+            'arguments' => [
+                'name' => 'default'
+            ],
+            'calls' => [
+                'pushHandler' => ['@default_logger_handler']
+            ]
+        ],
+        'default_logger_handler' => [
+            'class' => \Monolog\Handler\RotatingFileHandler::class,
+            'arguments' => [
+                'filename' => realpath(__DIR__ . '/../runtime') . '/default.log',
+                'maxFiles' => 7,
+                'level' => PHACT_DEBUG ? \Monolog\Logger::DEBUG : \Monolog\Logger::WARNING
             ]
         ],
         'event' => [
             'class' => \Phact\Event\EventManager::class
         ],
+        'session' => [
+            'class' => \Phact\Request\Session::class
+        ],
         'request' => [
-            'class' => \Phact\Request\RequestManager::class,
-            'httpRequest' => [
-                'class' => \Phact\Request\HttpRequest::class,
-                'session' => [
-                    'class' => \Phact\Request\Session::class
-                ]
-            ],
-            'cliRequest' => [
-                'class' => \Phact\Request\CliRequest::class,
-            ]
+            'class' => \Phact\Request\HttpRequest::class
+        ],
+        'cliRequest' => [
+            'class' => \Phact\Request\CliRequest::class
         ],
         'router' => [
             'class' => \Phact\Router\Router::class,
-            'pathRoutes' => 'base.config.routes',
-            'cacheTimeout' => PHACT_DEBUG ? null : 86400
+            'arguments' => [
+                'configPath' => 'base.config.routes'
+            ],
+            'properties' => [
+                'cacheTimeout' => PHACT_DEBUG ? null : 86400
+            ],
         ],
         'template' => [
             'class' => \Phact\Template\TemplateManager::class,
-            'librariesCacheTimeout' => PHACT_DEBUG ? null : 86400,
-            'forceCompile' => PHACT_DEBUG ? true : false,
-            'autoReload' => PHACT_DEBUG ? true : false
+            'properties' => [
+                'librariesCacheTimeout' => PHACT_DEBUG ? null : 86400,
+                'forceCompile' => PHACT_DEBUG ? true : false,
+                'autoReload' => PHACT_DEBUG ? true : false
+            ]
         ],
         'auth' => [
-            'class' => \Modules\User\Components\Auth::class
+            'class' => \Modules\User\Components\Auth::class,
+            'properties' => [
+                'afterLoginRoute' => 'personal:index',
+                'afterLogoutRoute' => 'user:login'
+            ]
         ],
         'storage'=>[
-            'class'=>\Phact\Storage\StorageManager::class,
-            'config'=>[
-                'default'=>[
-                    'class'=>\Phact\Storage\FileSystemStorage::class,
-                ]
-            ],
+            'class' => \Phact\Storage\FileSystemStorage::class
         ],
         'breadcrumbs' => [
             'class' => \Phact\Components\Breadcrumbs::class
@@ -116,50 +129,55 @@ return [
         ],
         'cache' => [
             'class' => \Phact\Cache\Cache::class,
-            'drivers' => [
-                'default' =>  [
-                    'class' => \Phact\Cache\Drivers\File::class
-                ]
+            'calls' => [
+                'setDrivers' => [[
+                    'default' =>  [
+                        'class' => \Phact\Cache\Drivers\File::class
+                    ]
+                ]]
             ]
         ],
         'mail' => [
             'class' => \Modules\Mail\Components\Mailer::class,
-            'defaultFrom' => 'EMAIL',
-            'mode' => 'smtp',
-            'config' => [
-                'host' => 'HOST',
-                'username' => 'EMAIL',
-                'password' => 'PASSWORD',
-                'port' => '465',
-                'security' => 'ssl'
+            'properties' => [
+                'defaultFrom' => 'order@rclass.pro',
+                'mode' => 'smtp',
+                'config' => [
+                    'host' => 'smtp.yandex.ru',
+                    'username' => 'order@rclass.pro',
+                    'password' => 'hZairr9APo',
+                    'port' => '465',
+                    'security' => 'ssl'
+                ]
             ]
         ],
         'assets' => [
             'class' => \Modules\Assets\Components\AssetsComponent::class,
-            'builds' => [
-                'default' => [
-                    'class' => \Modules\Assets\Builds\SimpleBuild::class,
-                    'publicPath' => '/static'
-                ],
-                'frontend' => [
-                    'class' => \Modules\Assets\Builds\ManifestBuild::class,
-                    'publicPath' => '/static',
-                    'manifestFile' => realpath( __DIR__ . '/../../www/static/manifest.json')
-                ],
-                'admin' => [
-                    'class' => \Modules\Assets\Builds\ManifestBuild::class,
-                    'publicPath' => '/static_admin',
-                    'manifestFile' => realpath(__DIR__ . '/../../www/static_admin/manifest.json')
-                ],
-                'modules' => [
-                    'class' => \Modules\Assets\Builds\SimpleBuild::class,
-                    'publicPath' => '/static_modules'
-                ]
+            'calls' => [
+                'setBuilds' => [[
+                    'default' => [
+                        'class' => \Modules\Assets\Builds\SimpleBuild::class,
+                        'publicPath' => '/static'
+                    ],
+                    'frontend' => [
+                        'class' => \Modules\Assets\Builds\ManifestBuild::class,
+                        'publicPath' => '/static',
+                        'manifestFile' => realpath( __DIR__ . '/../../www/static/manifest.json')
+                    ],
+                    'admin' => [
+                        'class' => \Modules\Assets\Builds\ManifestBuild::class,
+                        'publicPath' => '/static_admin',
+                        'manifestFile' => realpath(__DIR__ . '/../../www/static_admin/manifest.json')
+                    ],
+                    'modules' => [
+                        'class' => \Modules\Assets\Builds\SimpleBuild::class,
+                        'publicPath' => '/static_modules'
+                    ]
+                ]]
             ]
         ],
     ],
     'autoloadComponents' => [
-        'errorHandler',
-        'log'
+        'errorHandler'
     ]
 ];
